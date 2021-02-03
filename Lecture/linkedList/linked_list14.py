aa6ac8c8a1b64e96930441d5e7612cec2354f13e
16010948 <bjyoung0@gmail.com>
2021-02-03 18:07:20 +0900

    교차점 찾기(연결리스트 클래스 수정)

--- Lecture/linkedList/linked_list14.py
+++ Lecture/linkedList/linked_list14.py
@@ -3,86 +3,67 @@ class Node:
         self.data = d
         self.next = None
 
-class LinkedList:
-    def __init__(self):
-        self.header = Node(None)
+    def add_next(self, d):
+        if type(d) == int:
+            end = Node(d)
+            self.next = end
+            return end
+        else:
+            self.next = d
 
-    def append(self, d):
-        end = Node(d)
-        n = self.header
-        while n.next != None:
-            n = n.next
-        n.next = end
-
-    def delete(self, d):
-        n = self.header
-        while n.next != None:
-            if n.next.data == d:
-                n.next = n.next.next
-            else:
-                n = n.next
-
-    def retrieve(self):
-        n = self.header.next
+    def print(self):
+        n = self
         while n.next != None:
             print(n.data, end=" -> ")
             n = n.next
         print(n.data)
 
-    def get(self, k):
-        n = self.header
-        for _ in range(k):
-            if n == None:
-                return None
-            n = n.next
-        return n
-
     def size(self):
-        n = self.header
+        n = self
         total = 0
-        while n.next != None:
+        while n != None:
             total += 1
             n = n.next
         return total
 
-def get_intersection(l1, l2):
-    size1 = l1.size()
-    size2 = l2.size()
+    def get(self, k):
+        n = self
+        for _ in range(k):
+            n = n.next
+        return n
+
+def get_intersection(n1, n2):
+    size1 = n1.size()
+    size2 = n2.size()
 
     if size1 > size2:
-        l1 = l1.get(size1 - size2)
-        l2 = l2.header
+        n1 = n1.get(size1 - size2)
     elif size2 > size1:
-        l2 = l2. get(size2 - size1)
-        l1 = l1.header
+        n2 = n2. get(size2 - size1)
 
-    while l1 != None and l2 != None:
-        if l1.data == l2.data:
-            return l1
-        l1 = l1.next
-        l2 = l2.next
+    while n1 != None and n2 != None:
+        if n1 == n2:
+            return n1
+        n1 = n1.next
+        n2 = n2.next
 
     return None
 
 
-l1 = LinkedList()
-l1.append(5)
-l1.append(7)
-l1.append(9)
-l1.append(10)
-l1.append(7)
-l1.append(6)
-l1.retrieve()
+n1 = Node(5)
+n2 = n1.add_next(7)
+n3 = n2.add_next(9)
+n4 = n3.add_next(10)
+n5 = n4.add_next(7)
+n6 = n5.add_next(6)
+n1.print()
 
-l2 = LinkedList()
-l2.append(1)
-l2.append(2)
-l2.append(3)
-l2.append(4)
-l2.append(5)
-l2.retrieve()
+m1 = Node(6)
+m2 = m1.add_next(8)
+m2.add_next(n4)
+m1.print()
 
-n = get_intersection(l1, l2)
+n = get_intersection(n1, m1)
 if n != None:
     print("Intersection:", n.data)
 else:
