35e212c792b652942da74db96541135e3050cde3
16010948 <bjyoung0@gmail.com>
2021-02-17 09:36:26 +0900

    배열 A, B에서 각각 한 개의 숫자를 뽑아 두 수를 곱하여 누적된 값을 최소가 되도록 만들기

--- Programmers/practice9.py
+++ Programmers/practice9.py
@@ -1,36 +1,10 @@
-def quick_sort(arr, start, end):
-    if start >= end:
-        return
-    pivot = start
-    left = start + 1
-    right = end
-
-    while left <= right:
-        while left <= end and arr[pivot] >= arr[left]:
-            left += 1
-        while right > start and arr[pivot] <= arr[right]:
-            right -= 1
-        if left >= right:
-            arr[pivot], arr[right] = arr[right], arr[pivot]
-        else:
-            arr[left], arr[right] = arr[right], arr[left]
-    quick_sort(arr, start, right - 1)
-    quick_sort(arr, right + 1, end)
-
-
 def solution(A, B):
     answer = 0
-    # quick_sort(A, 0, len(A) - 1)
-    # quick_sort(B, 0, len(B) - 1)
     A.sort()
-    B.sort()
-    a_index = 0
-    b_index = len(B) - 1
+    B.sort(reverse=True)
 
-    while a_index <= len(A) - 1 and b_index >= 0:
-        answer += A[a_index] * B[b_index]
-        a_index += 1
-        b_index -= 1
+    for i in range(len(A)):
+        answer += A[i] * B[i]
 
     return answer
 
