7148976d5e164b263281fefc06f389f005a4a627
16010948 <bjyoung0@gmail.com>
2021-02-18 23:15:44 +0900

    문자열의 길이가 4 혹은 6이고, 숫자로만 구성돼있는지 확인해주는 함수

---
+++ Programmers/practice13.py
@@ -0,0 +1,14 @@
+def solution(s):
+    answer = True
+    if (len(s) != 4 and len(s) != 6) or not s.isdigit():
+        answer = False
+
+    return answer
+
+s = "a234"
+# answer = false
+print(solution(s))
+
+s = "1234"
+# answer = true
+print(solution(s))
