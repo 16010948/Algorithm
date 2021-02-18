0867e9ab1b57fa4ec4ca9037dad403d090732a0a
16010948 <bjyoung0@gmail.com>
2021-02-18 23:40:01 +0900

    num이 짝수일 경우 "Even", 홀수인 경우 "Odd"를 반환

---
+++ Programmers/practice17.py
@@ -0,0 +1,11 @@
+def solution(num):
+    answer = "Even" if num % 2 == 0 else "Odd"
+    return answer
+
+num = 3
+# answer = "Odd"
+print(solution(num))
+
+num = 4
+# answer = "Even"
+print(solution(num))
