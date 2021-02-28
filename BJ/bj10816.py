ee5e5cf81268c8655001be26aa7f83ea2c57b3aa
16010948 <bjyoung0@gmail.com>
2021-02-28 22:02:46 +0900

    S = A[0] * B[0] + ... + A[N - 1] * B[N - 1] S의 값을 가장 작게 만들기 위해 A의 수를 재배열

--- BJ/bj10816.py
+++ BJ/bj10816.py
@@ -7,6 +7,15 @@ def bisect_left(array, start, end, target):
             start = mid + 1
     return end
 
+def bisect_right(array, start, end, target):
+    while start < end:
+        mid = (start + end) // 2
+        if target >= array[mid]:
+            start = mid + 1
+        else:
+            end = mid
+    return start
+
 n = int(input())
 card1 = list(map(int, input().split()))
 card1.sort()
@@ -15,9 +24,13 @@ m = int(input())
 card2 = list(map(int, input().split()))
 
 for card_num in card2:
-    index = bisect_left(card1, 0, n - 1, card_num)
-    count = 0
-    while index < n and card1[index] == card_num:
-        count += 1
-        index += 1
-    print(count, end=" ")
+    index_left = bisect_left(card1, 0, n - 1, card_num)
+    index_right = bisect_right(card1, 0, n - 1, card_num)
+    count = index_right - index_left
+    if card1[index_left] != card_num:
+        print(0, end=" ")
+    else:
+        if index_right == n - 1:
+            print(count + 1, end=" ")
+        else:
+            print(count, end=" ")
