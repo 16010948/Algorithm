e028ede58415d20da8c3c1a0783f5565f419713d
16010948 <bjyoung0@gmail.com>
2021-03-01 20:37:18 +0900

    중복 빼고 오름차순으로 정렬

---
+++ BJ/bj10867.py
@@ -0,0 +1,37 @@
+def merge_sort(array):
+    n = len(array)
+    if n <= 1:
+        return
+    mid = n // 2
+    g1 = array[:mid]
+    g2 = array[mid:]
+    merge_sort(g1)
+    merge_sort(g2)
+
+    i1 = 0
+    i2 = 0
+    ia = 0
+    while i1 < len(g1) and i2 < len(g2):
+        if g1[i1] < g2[i2]:
+            array[ia] = g1[i1]
+            i1 += 1
+        else:
+            array[ia] = g2[i2]
+            i2 += 1
+        ia += 1
+
+    for i in range(i1, len(g1)):
+        array[ia] = g1[i]
+        ia += 1
+
+    for i in range(i2, len(g2)):
+        array[ia] = g2[i]
+        ia += 1
+
+n = int(input())
+array = list(map(int, input().split()))
+deduplicated = list(set(array))
+merge_sort(deduplicated)
+
+for item in deduplicated:
+    print(item, end=" ")
