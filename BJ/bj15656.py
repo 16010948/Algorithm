ad1aa55c60fe734c99a7f4007ba27171afbfe832
16010948 <bjyoung0@gmail.com>
2021-07-20 14:30:38 +0900

    N과 M (7) N개의 자연수와 자연수 M이 주어졌을 때, 아래 조건을 만족하는 길이가 M인 수열을 모두 구하는 프로그램을 작성하시오. N개의 자연수는 모두 다른 수이다. N개의 자연수 중에서 M개를 고른 수열 같은 수를 여러 번 골라도 된다.

---
+++ BJ/bj15656.py
@@ -0,0 +1,45 @@
+def merge_sort(array):
+    length = len(array)
+    if length <= 1:
+        return
+    mid = length // 2
+    g1 = array[:mid]
+    g2 = array[mid:]
+    merge_sort(g1)
+    merge_sort(g2)
+
+    i1 = 0
+    i2 = 0
+    ia = 0
+    while i1 < len(g1) and i2 < len(g2):
+        if g1[i1] < g2[i2]:
+            array[ia] = g1[i1]
+            i1 += 1
+        else:
+            array[ia] = g2[i2]
+            i2 += 1
+        ia += 1
+    for i in range(i1, len(g1)):
+        array[ia] = g1[i]
+        ia += 1
+    for i in range(i2, len(g2)):
+        array[ia] = g2[i]
+        ia += 1
+
+def dfs(array):
+    if len(array) == m:
+        for data in array:
+            print(data, end=" ")
+        print()
+        return
+
+    for num in nums:
+        array.append(num)
+        dfs(array)
+        array.pop()
+
+
+n, m = map(int, input().split())
+nums = list(map(int, input().split()))
+merge_sort(nums)
+dfs([])
