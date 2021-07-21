2ca23133b2295688a0c3032af73fa09267acf3ab
16010948 <bjyoung0@gmail.com>
2021-07-21 17:01:40 +0900

    가장 긴 증가하는 부분 수열 열 A가 주어졌을 때, 가장 긴 증가하는 부분 수열을 구하는 프로그램

--- BJ/bj11053.py
+++ BJ/bj11053.py
@@ -1,18 +1,23 @@
+def bisec_left(array, target, n):
+    start = 0
+    end = n
+    while start < end:
+        mid = (start + end) // 2
+        if target > array[mid]:
+            start = mid + 1
+        else:
+            end = mid
+    return end
 n = int(input())
 arr = list(map(int, input().split()))
 
-dp = [0] * n
-count = [0] * n
-dp[0] = arr[0]
-count[0] = 1
-max_count = 0
-for i in range(1, n):
-    if arr[i - 1] < arr[i]:
-        dp[i] = arr[i]
-        count[i] = count[i - 1] + 1
+stack = []
+length = -1
+for i in range(n):
+    if length < 0 or stack[length] < arr[i]:
+        length += 1
+        stack.append(arr[i])
     else:
-        dp[i] = arr[i - 1]
-        count[i] = count[i - 1]
-
-print(dp)
-print(count)
+        position = bisec_left(stack, arr[i], length)
+        stack[position] = arr[i]
+print(length + 1)
