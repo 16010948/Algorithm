5e6d039078d03fb515f2d074a63c4be390e8f2ad
16010948 <bjyoung0@gmail.com>
2021-07-21 15:07:31 +0900

    중간값 찾기 병합 정렬 사용

--- SW/sw2063.java
+++ SW/sw2063.java
@@ -2,25 +2,45 @@ import java.util.Scanner;
 
 class Solution
 {
+    static int[] array;
+    static int N;
+    static void mergeSort(int start, int end) {
+        if(start >= end){
+            return ;
+        }
+    	int mid = (start + end) / 2;
+        mergeSort(start, mid);
+        mergeSort(mid + 1, end);
+
+        int i1 = start;
+        int i2 = mid + 1;
+        int ia = start;
+        int[] tmp = new int[N];
+
+        while(i1 <= mid || i2 <= end) {
+            if(i2 > end || (i1 <= mid && array[i1] < array[i2])){
+            	tmp[ia++] = array[i1++];
+            } else {
+                tmp[ia++] = array[i2++];
+            }
+        }
+
+        for(int i = start ; i <= end; i++){
+            array[i] = tmp[i];
+        }
+    }
+
 	public static void main(String args[]) throws Exception
 	{
 		Scanner sc = new Scanner(System.in);
-		int N=sc.nextInt();
+		N=sc.nextInt();
 
-        int[] count = new int[101];
+        array = new int[N];
         for(int i = 0; i < N; i++)
 		{
-			int score = sc.nextInt();
-            count[score]++;
+			array[i] = sc.nextInt();
 		}
-
-        int countSum = 0;
-        for(int i = 0; i <= 100; i++){
-            countSum += count[i];
-            if(countSum >= (N / 2) + 1) {
-                System.out.println(i);
-                break;
-            }
-        }
+        mergeSort(0, N - 1);
+        System.out.println(array[N / 2]);
 	}
 }
