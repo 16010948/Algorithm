c5bd3359573a6727e2fd97fc64cd01c9ddab78c3
16010948 <bjyoung0@gmail.com>
2021-08-04 13:36:03 +0900

    임시 반장 정하기 자기반 학생 중에서 1학년부터 5학년까지 지내오면서 한번이라도 같은 반이었던 사람이 가장 많은 학생을 임시 반장으로 정하려 한다. 각 학생들이 1학년부터 5학년까지 속했던 반이 주어질 때, 임시 반장을 정하는 프로그램

--- BJ/bj1268.py
+++ BJ/bj1268.py
@@ -1,22 +1,22 @@
 n = int(input())
 
-school_classes = [[] for _ in range(5)]
+classes = [{} for _ in range(5)]
 for i in range(n):
-    school_class = list(map(int, input().split()))
-    for j in range(5):
-        school_classes[j].append(school_class[j])
+    students = list(map(int, input().split()))
+    for grade in range(5):
+        if students[grade] in classes[grade]:
+            classes[grade][students[grade]].append(i + 1)
+        else:
+            classes[grade][students[grade]] = [i + 1]
 
-students = [set()] * n
-# for i in range(5):
-#     for j in range(n):
-#         if school_classes[i][j] in headcount.keys():
-#             students[j].append(j)
-#         else:
-#             students[school_classes[i][j]] = [j]
-#     print(headcount)
-    # for j in range(n):
-    #     students[j] += headcount[school_classes[i][j]]
-    #     if max_headcount < students[j]:
-    #         max_headcount = students[j]
-    #         class_leader = j
-# print(class_leader + 1)
+friends = [set() for _ in range(n + 1)]
+max_friends = 0
+leader = 0
+for dic in classes:
+    for key in dic:
+        for c in dic[key]:
+            friends[c] = friends[c].union(set(dic[key]))
+            if len(friends[c]) > max_friends:
+                max_friends = len(friends[c])
+                leader = c
+print(leader)
