4cbc3b5a1fcdf177b527fad5f4e48a591a6bc422
16010948 <bjyoung0@gmail.com>
2021-08-18 11:12:10 +0900

    단지번호붙이기 정사각형 모양의 지도가 있다. 1은 집이 있는 곳을, 0은 집이 없는 곳을 나타낸다. 철수는 이 지도를 가지고 연결된 집의 모임인 단지를 정의하고, 단지에 번호를 붙이려 한다. 여기서 연결되었다는 것은 어떤 집이 좌우, 혹은 아래위로 다른 집이 있는 경우를 말한다.  지도를 입력하여 단지수를 출력하고, 각 단지에 속하는 집의 수를 오름차순으로 정렬하여 출력하는 프로그램

---
+++ BJ/bj2667..java
@@ -0,0 +1,53 @@
+package com.ssafy.algo;
+
+import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class BJ2667 {
+	static int N;
+	static char[][] map;
+	static List<Integer> sizes;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+		map = new char[N][N];
+
+		for(int i = 0; i < N; i++) {
+			map[i] = sc.next().toCharArray();
+		}
+
+		sizes = new ArrayList<>();
+		for(int i = 0; i < N; i++) {
+			for(int j = 0; j < N; j++) {
+				if(map[i][j] == '1') {
+					sizes.add(0);
+					dfs(i,j);
+				}
+			}
+		}
+
+		Collections.sort(sizes);
+		System.out.println(sizes.size());
+		for(int size: sizes) {
+			System.out.println(size);
+		}
+	}
+
+	static int[][] deltas = {{-1, 0}, {0, -1}, {0, 1}, {1, 0}};
+	static void dfs(int r, int c) {
+		map[r][c] = '0';
+		sizes.set(sizes.size()-1, sizes.get(sizes.size() - 1) + 1);
+		for(int i = 0; i < 4; i++) {
+			int nr = r + deltas[i][0];
+			int nc = c + deltas[i][1];
+
+			if(nr >= 0 && nr < N && nc >= 0 && nc < N && map[nr][nc] == '1') {
+				dfs(nr, nc);
+			}
+
+		}
+	}
+}
