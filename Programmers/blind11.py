7d75916c08bdbb3b9cf7b9040fc6afca8b6b43ab
16010948 <bjyoung0@gmail.com>
2021-09-01 19:02:19 +0900

    메뉴 리뉴얼 기존에는 단품으로만 제공하던 메뉴를 조합해서 코스요리 형태로 재구성해서 새로운 메뉴를 제공하기로 결정했습니다. 어떤 단품메뉴들을 조합해서 코스요리 메뉴로 구성하면 좋을 지 고민하던 "스카피"는 이전에 각 손님들이 주문할 때 가장 많이 함께 주문한 단품메뉴들을 코스요리 메뉴로 구성하기로 했습니다. 단, 코스요리 메뉴는 최소 2가지 이상의 단품메뉴로 구성하려고 합니다. 또한, 최소 2명 이상의 손님으로부터 주문된 단품메뉴 조합에 대해서만 코스요리 메뉴 후보에 포함하기로 했습니다. 각 손님들이 주문한 단품메뉴들이 문자열 형식으로 담긴 배열 orders, "스카피"가 추가하고 싶어하는 코스요리를 구성하는 단품메뉴들의 갯수가 담긴 배열 course가 매개변수로 주어질 때, "스카피"가 새로 추가하게 될 코스요리의 메뉴 구성을 문자열 형태로 배열에 담아 return 하도록 solution 함수

---
+++ Programmers/blind11.py
@@ -0,0 +1,76 @@
+max_value = 0
+orders_dic = {}
+max_orders = []
+
+
+def combination(start, order, result, n):
+    global orders_dic, max_value, max_orders
+    if len(result) == n:
+        if result in orders_dic:
+            orders_dic[result] += 1
+        else:
+            orders_dic[result] = 1
+
+        if orders_dic[result] >= 2:
+            if orders_dic[result] > max_value:
+                max_value = orders_dic[result]
+                max_orders = [result]
+            elif orders_dic[result] == max_value:
+                max_orders.append(result)
+        return
+
+    for i in range(start, len(order)):
+        combination(i + 1, order, result + order[i], n)
+
+
+def merge_sort(array):
+    length = len(array)
+    if length <= 1:
+        return
+    mid = length // 2
+    g1 = array[:mid]
+    g2 = array[mid:]
+    merge_sort(g1)
+    merge_sort(g2)
+
+    i1 = 0
+    i2 = 0
+    ia = 0
+    while i1 < len(g1) or i2 < len(g2):
+        if i2 >= len(g2) or (i1 < len(g1) and g1[i1] < g2[i2]):
+            array[ia] = g1[i1]
+            i1 += 1
+        else:
+            array[ia] = g2[i2]
+            i2 += 1
+        ia += 1
+
+
+def solution(orders, course):
+    global orders_dic, max_value, max_orders
+    answer = []
+
+    for c in course:
+        max_value = 0
+        orders_dic = {}
+        max_orders = []
+        for o in orders:
+            tmp = list(o)
+            merge_sort(tmp)
+            combination(0, tmp, "", c)
+        answer += max_orders
+    print(answer)
+
+    return answer
+
+orders = ["ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH"]
+course = [2, 3, 4]
+print(solution(orders, course))
+
+orders = ["ABCDE", "AB", "CD", "ADE", "XYZ", "XYZ", "ACD"]
+course = [2, 3, 5]
+print(solution(orders, course))
+
+orders = ["XYZ", "XWY", "WXA"]
+course = [2, 3, 4]
+print(solution(orders, course))
