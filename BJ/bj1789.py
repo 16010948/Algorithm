65fb9c2c95cef91a9c73220583017be2d20fe2df
16010948 <bjyoung0@gmail.com>
2021-12-25 15:59:57 +0900

    수들의 합 - 이분탐색을 이용하여 구현

---
+++ BJ/bj1789.py
@@ -0,0 +1,13 @@
+s = int(input())
+
+start = 1
+end = s
+answer = 1
+while start <= end:
+    mid = (start + end) // 2
+    if (mid * (mid + 1)) // 2 <= s:
+        answer = mid
+        start = mid + 1
+    else:
+        end = mid - 1
+print(answer)
