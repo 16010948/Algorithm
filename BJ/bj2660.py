d0a9247e16d4de392c54c834ea6a6dbded5fb156
16010948 <bjyoung0@gmail.com>
2022-01-03 08:49:45 +0900

    회장뽑기 - 그래프를 이용하여 플로이드-와샬 알고리즘 구현 - 현재 위치에서 갈 수 있는 거리 중 가장 먼 곳이 점수가 됨

--- BJ/bj2660.py
+++ BJ/bj2660.py
@@ -1,20 +1,31 @@
+INF = int(1e9)
 n = int(input())
 
-score = [n] * (n + 1)
-min_score = n
+graph = [[INF] * (n + 1) for _ in range(n + 1)]
 while True:
     candidate1, candidate2 = map(int, input().split())
     if candidate1 < 0 and candidate2 < 0:
         break
 
-    score[candidate1] -= 1
-    score[candidate2] -= 1
-    min_score = min(min_score, score[candidate1], score[candidate2])
+    graph[candidate1][candidate2] = graph[candidate2][candidate1] = 1
 
 
+for k in range(1, n + 1):
+    for i in range(1, n + 1):
+        graph[i][i] = 0
+        for j in range(1, n + 1):
+            if graph[i][j] > graph[i][k] + graph[k][j]:
+                graph[i][j] = graph[i][k] + graph[k][j]
+min_score = n
 answer = []
 for i in range(1, n + 1):
-    if score[i] == min_score:
+    max_score = 0
+    for j in range(1, n + 1):
+        max_score = max(max_score, graph[i][j])
+    if max_score < min_score:
+        min_score = max_score
+        answer = [i]
+    elif max_score == min_score:
         answer.append(i)
 
 print(min_score, len(answer))
